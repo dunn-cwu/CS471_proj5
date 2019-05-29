565ed13600407c2a2200453f8ab92048c82b3da6
Andrew Dunn <DunnA@cwu.edu>
2019-05-29 11:49:26 -0700

    Started adding debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,15 @@
  * 
  */
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <set>
+
 #include "experiment.h"
 
 using namespace std;
+
+int runDebugJobSeq(const char* seq);
 
 int main(int argc, char** argv)
 {
@@ -20,22 +26,59 @@ int main(int argc, char** argv)
     if (argc <= 1)
     {
         cout << "Error: Missing command line parameter." << endl;
-        cout << "Proper usage: " << argv[0] << " [param file]" << endl;
+        cout << "Proper usage: " << argv[0] << " [param file] \"[Debug Job Sequence]\"" << endl;
+        cout << "The debug job sequence is optional, and must be passed in the form \"1 2 3 4 5\" as a single argument, where the values are the jobs separated by spaces." << endl;
         return EXIT_FAILURE;
     }
 
-    try
+    if (argc > 2)
     {
-        // Run experiment and return error code
-        cs471::Experiment ex(argv[1]);
-        return ex.run();
+        return runDebugJobSeq(argv[2]);
     }
-    catch(const std::exception& e)
+    else
     {
-        std::cerr << "An exception occured:" << endl;
-        std::cerr << e.what() << endl;
+        try
+        {
+            // Run experiment and return error code
+            cs471::Experiment ex(argv[1]);
+            return ex.run();
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "An exception occured:" << endl;
+            std::cerr << e.what() << endl;
+        }
     }
     
+    return 0;
+}
+
+int runDebugJobSeq(const char* seq)
+{
+    string strSeq = seq;
+    vector<int> jobSeq;
+
+    stringstream ss(strSeq);
+    int val;
+
+    while (ss >> val)
+    {
+        jobSeq.push_back(val);
+    }
+
+    if (jobSeq.size() == 0)
+    {
+        cerr << "Error: debug job sequence is missing or invalid." << endl;
+        return 1;
+    }
+
+    set<int> permCheckSet(jobSeq.begin(), jobSeq.end());
+    if (permCheckSet.size() != jobSeq.size())
+    {
+        cerr << "Error: debug job sequence has duplicate jobs in permutation." << endl;
+        return 2;
+    }
+
     return 0;
 }
 
